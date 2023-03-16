1a087ff55fec955db8be3276a8c7d17421e76ecc
SLEM <artem.gembitskiy@mail.ru>
2023-03-16 21:02:42 +0200

    Создан класс Chat

---
+++ Программа/PPPI/PPPI/Chat.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace PPPI
+{
+    //Класс для чата в мультиплеерной сессии.
+    internal class Chat
+    {
+        
+    }
+}
