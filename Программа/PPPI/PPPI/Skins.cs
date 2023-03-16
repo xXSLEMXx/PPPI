434e083d69498d8b5d636c4853cbad2fe5d0a1d3
SLEM <artem.gembitskiy@mail.ru>
2023-03-16 20:51:52 +0200

    Создан класс Skins

---
+++ Программа/PPPI/PPPI/Skins.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPPI
+{
+    //Класс для изменения внешнего вида блоков и игрового поля.
+    internal class Skins
+    {
+        
+    }
+}
