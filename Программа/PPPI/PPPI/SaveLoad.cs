9e4fe2537f6c851dd37e781ae0d352a31b359f85
SLEM <artem.gembitskiy@mail.ru>
2023-03-16 20:57:59 +0200

    Создан класс SaveLoad

---
+++ Программа/PPPI/PPPI/SaveLoad.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPPI
+{
+    //Класс сохранения и загрузки прогресса.
+    internal class SaveLoad
+    {
+        
+    }
+}
