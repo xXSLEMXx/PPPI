920c2f84823c6b09db6d7c9ab46f04f59393ef95
SLEM <artem.gembitskiy@mail.ru>
2023-03-16 19:55:29 +0200

    Добавлены переменные текущего положжения блока

--- Программа/PPPI/PPPI/Block.cs
+++ Программа/PPPI/PPPI/Block.cs
@@ -9,6 +9,7 @@ namespace PPPI
     //Класс падающего блока.
     internal class Block
     {
-       
+        public int x;   //Положение блока по x.
+        public int y;   //Положение блока по y.
     }
 }
