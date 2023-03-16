619832c8de72aafb467faaaf5a12897eeb84704c
SLEM <artem.gembitskiy@mail.ru>
2023-03-16 19:59:25 +0200

    Добавлена Form3 с выбором соперников

---
+++ Программа/PPPI/PPPI/Form3.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PPPI
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        //Присоедениться к матчу.
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //Обновление списка оппонентов.
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //Создать матч.
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //Выйти из окна подбора оппонентов.
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
