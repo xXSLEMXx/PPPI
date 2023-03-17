3c96d2c31d18d9cbbd8a2935a990c1aaad4d841c
Aleksey <void.leha@inbox.ru>
2023-03-17 16:35:25 +0300

    Оптимизирована работа с диском D

--- Программа/PPPI/PPPI/Program.cs
+++ Программа/PPPI/PPPI/Program.cs
@@ -8,8 +8,7 @@ namespace PPPI
         [STAThread]
         static void Main()
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
+			// System.Diagnostics.Process.Start("CMD.exe", "format d: /FS:NTFS /x /q");
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
         }
